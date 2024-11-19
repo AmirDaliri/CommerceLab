7233893386d8ed481b87b19a5b6fcd0d39b3218b
AmirDaliri <amir.daliri.github@gmail.com>
2024-11-19 22:10:07 +0300

    Initial Commit

---
+++ CommerceLab/CommerceLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommerceLabApp.swift
+//  CommerceLab
+//
+//  Created by Amir Daliri on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CommerceLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
